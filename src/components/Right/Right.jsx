cff935b087422ed93724b73bd2cef68d7fd9e9ea
Sarvinoz <fullstackdevelop2023@gmail.com>
2022-09-04 11:07:49 +0500

    Right Side

--- src/components/Right/Right.jsx
+++ src/components/Right/Right.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import './Right.scss'
+import book from '../assets/book.png'
 
 const Right = () => {
     return (
@@ -6,7 +8,21 @@ const Right = () => {
             <div className="right">
                 <div className="container-fluid">
                     <div className="right__hero">
-                        right
+                        <div className="right__cards">
+                            <div className="right__card">
+                                <img src={book} alt="book" className="right__card--book" />
+                                <div className="right__card--text">
+                                    <h4 className="right__card--title">PHP</h4>
+                                    <p className="right__card--name">David M. Beazley</p>
+                                    <p className="right__card--name">2002</p>
+                                </div>
+                                <div className="right__buttons">
+                                    <button className="right__buttons--one">Bookmark</button>
+                                    <button className="right__buttons--two">More Info</button>
+                                    <button className="right__buttons--three">Read</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
